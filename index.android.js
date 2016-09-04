a4344ae0ccf404d99c3455ee084d6712eb0cc238
Bernardo Smaniotto <be.smaniotto@gmail.com>
2016-09-04 14:18:07 -0300

    Initialize react native application.

---
+++ index.android.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+} from 'react-native';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        //Start app development here!
+      </View>
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('GroceriesList', () => App);
